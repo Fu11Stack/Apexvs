4b0ddca8facc6c34dbd139f2c42e12671f1025f7
Fu11Stack <96635777+Fu11Stack@users.noreply.github.com>
2022-05-10 23:03:20 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Menu from './components/Menu';
+import './App.css';
+
+function App() {
+  return (
+    <Menu />
+  );
+}
+
+export default App;
---
+++ src/components/Menu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Menu = () => {
+  return (
+   <div>
+    <button />
+   </div>
+
+  );
+}
+
+export default Menu;
