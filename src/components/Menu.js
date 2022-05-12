2066bca11315e693da8bab361328d7c8c1f77581
Fu11Stack <96635777+Fu11Stack@users.noreply.github.com>
2022-05-11 22:58:01 -0700

    Add files via upload

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import styled from "styled-components";
+
+const Play = styled.button`
+display: inline-block;
+background-color: red;
+width: 199px;
+height: 120px;
+text-decoration: none;
+color: white;
+font-size: 27px;
+font-weight: 700;
+letter-spacing: 1px;
+text-align: center;
+`;
 
 const Menu = () => {
   return (
-   <div>
-    <button />
-   </div>
+   
+    <Play />
+   
 
   );
-}
+};
 
 export default Menu;
